ce0a3c47c7f6af28f67c32fe23598a2792965224
Emmanuel Gonzalez <emmanuel.gonzalez22@upr.edu>
2023-11-07 21:42:53 -0400

    Initial project setup

---
+++ src/main.asm
@@ -0,0 +1,30 @@
+.include "constants.inc"
+.include "header.inc"
+
+.segment "CODE"
+.proc irq_handler
+  RTI
+.endproc
+
+.proc nmi_handler
+  RTI
+.endproc
+
+.import reset_handler
+
+.export main
+.proc main
+  LDX PPUSTATUS
+  LDX #$3f
+  STX PPUADDR
+  LDX #$00
+  STX PPUADDR
+  LDA #$29
+  STA PPUDATA
+  LDA #%00011110
+  STA PPUMASK
+  ; contents of main here
+.endproc
+
+.segment "VECTORS"
+.addr nmi_handler, reset_handler, irq_handler
