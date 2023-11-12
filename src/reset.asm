2b9d6093cd414f6031e9dce7624ff0a91aa637fb
Emmanuel Gonzalez <emmanuel.gonzalez22@upr.edu>
2023-11-12 16:16:07 -0400

    Staging backgrounds changes

--- src/reset.asm
+++ src/reset.asm
@@ -14,6 +14,16 @@
         BIT PPUSTATUS
         BPL vblankwait
         JMP main
+
+    LDX #$00
+    LDA #$ff
+    clear_oam:
+        STA $0200,X ; set sprite y-positions off the screen
+        INX
+        INX
+        INX
+        INX
+        BNE clear_oam
 .endproc
 
 ; menos queja, mas tolerancia -Wichie
